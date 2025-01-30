847119a7410cb33f4bbb53d43407a0efe161c562
subaru <e245711@ie.u-ryukyu.ac.jp>
2025-01-30 13:15:10 +0900

    other

--- mian.c
+++ mian.c
@@ -3,15 +3,14 @@
 void Number_sort(int arr[], int size);
 
 int main(){
-    const int SIZE = 5; //定数として配列のサイズを宣言                          
-    int scores[SIZE] = {0, 60, 70, 100, 90};
+    const int SIZE = 6; //定数として配列のサイズを宣言                          
+    int scores[SIZE] = {100, 60, 70, 100, 90, 80};
     //処理コード
 
     Number_sort(scores, SIZE);  // ソート関数を呼び出し
     
     for (int i = 0; i < SIZE; i++) {
         printf("%d ", scores[i]);
-    
     }
 
     printf("\n"); 
@@ -19,9 +18,9 @@ int main(){
 
 
 void Number_sort(int arr[], int size){
-    for (int i = 0; i < size - 1; i++) {  // 外側のループ（ソートの回数）
-        for (int j = 0; j < size - 1 - i; j++) {  // 内側のループ（隣同士の比較）
-            if (arr[j] < arr[j + 1]) {  // 大きい値を前に移動
+    for (int i = 0; i < size - 1; i++) {  
+        for (int j = 0; j < size - 1 - i; j++) {  
+            if (arr[j] < arr[j + 1]) {  
                 int temp = arr[j];  
                 arr[j] = arr[j + 1];  
                 arr[j + 1] = temp;  
